ba508c4b9831e9572ff8ab01c6ad065b0a7c1109
lakmalmabm <lakmalmabm@gmail.com>
2024-06-21 10:22:35 +0530

    fix issue in document page

---
+++ components/documents/index.tsx
@@ -0,0 +1,296 @@
+"use client";
+
+import * as React from "react";
+import { RxCaretSort } from "react-icons/rx";
+import { HiDotsHorizontal } from "react-icons/hi";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { supabase } from "@/lib/supabase-client";
+import FileUploadButton from "@/components/documents/add-documents";
+import Loading from "@/components/ui/loading";
+import { deleteDocument } from "@/lib/deleteDocuments"; // Import the deleteDocument function
+import { toast } from "react-toastify";
+
+export type Document = {
+  id: string;
+  size: number;
+  status: "success" | "failed" | "processing";
+  name: string;
+};
+
+const bytesToMB = (bytes: number) => (bytes / (1024 * 1024)).toFixed(2);
+
+export const columns: ColumnDef<Document>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("status")}</div>
+    ),
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="p-0 m-0 items-center flex justify-start hover:bg-background"
+        >
+          Name
+          <RxCaretSort className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "size",
+    header: () => <div className="">Size</div>,
+    cell: ({ row }) => {
+      const sizeInBytes = parseFloat(row.getValue("size"));
+
+      return <div className="font-medium">{bytesToMB(sizeInBytes)} MB</div>;
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const document = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <HiDotsHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem className="bg-none hover:bg-none">
+              <Button
+              className="w-full"
+              variant={"destructive"}
+              onClick={async () => {
+                try {
+                  // Delete the document from the database
+                  await deleteDocument(document.name);
+
+                  console.log("Document deleted successfully");
+                  toast.success('Document deleted successfully')
+                } catch (error) {
+                  console.error("Error deleting document:", error);
+                  toast.error("Error deleting document!")
+                }
+              }}>
+                Delete
+              </Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export default function Documents() {
+  const [data, setData] = React.useState<Document[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("document_info").select("*");
+      if (error) {
+        console.error("Error fetching data: ", error);
+      } else {
+        setData(data);
+      }
+      setLoading(false);
+    };
+
+    // Set up Supabase real-time subscription
+    const insertChannel = supabase.channel("document_info");
+
+    insertChannel
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "document_info" },
+        fetchData
+      )
+      .subscribe();
+
+    fetchData();
+  }, []);
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  if (loading)
+    return <Loading loadingText="Loading documents" />;
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center justify-between py-4 w-full">
+        <Input
+          placeholder="Filter documents..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <FileUploadButton />
+      </div>
+      <div className="rounded-md border">
+        <div className="max-h-[500px] min-h-12 overflow-y-auto scrollbar-default">
+          {/* Add this wrapper */}
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody className="max-h-96 overflow-y-auto scrollbar-default">
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
